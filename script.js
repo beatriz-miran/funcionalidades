605da7080325083a32b3a600d3c56dd22f55c4ae
2830482411044 <2830482411044@fatecsdp.acad.local>
2024-12-16 12:22:19 -0300

    geradordesenha

---
+++ script.js
@@ -0,0 +1,18 @@
+//GERADOR DE SENHA
+
+//Componentes
+let campoSenha = document.getElementById('campo-senha')
+let botao = document.getElementById('botao-gerar')
+let regSenha = '!@#$%&*0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@'
+
+botao.addEventListener('click', ()=>{
+    
+    let senhaAtual = ''
+
+
+    for(let i = 0; i < 20; i++){
+        senhaAtual += regSenha[Math.floor(Math.random() * regSenha.length)]
+    }
+
+    campoSenha.value = senhaAtual
+})
